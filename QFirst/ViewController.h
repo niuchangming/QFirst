3bff451ed00e3629780e8f5cbc2e0f07eeff948a
ChangmingNiu <niu2yue@gmail.com>
2016-06-12 11:03:17 +0800

    Initial Commit

---
+++ QFirst/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QFirst
+//
+//  Created by ChangmingNiu on 12/6/16.
+//  Copyright © 2016 EKOO LAB PTE. LTD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ QFirst/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  QFirst
+//
+//  Created by ChangmingNiu on 12/6/16.
+//  Copyright © 2016 EKOO LAB PTE. LTD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
